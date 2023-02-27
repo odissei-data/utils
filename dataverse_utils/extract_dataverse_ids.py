d882495fd82701580375cec7952b76ab49e5d0f7
Fjodor van Rijsselberg <fjodorvanrijsselberg@Fjodors-MBP.ia.knaw.local>
2023-02-27 11:28:21 +0100

    Changed the publish dataverse script to only publish unpublished datasets and not bump already published datasets.

--- dataverse_utils/extract_dataverse_ids.py
+++ dataverse_utils/extract_dataverse_ids.py
@@ -1,6 +1,6 @@
 import requests
 
-from Licenses.config import PRODUCTION_API_TOKEN, API_TOKEN, ROOT
+from config import API_TOKEN, ROOT
 
 
 def extract_dataverse_ids(dataverse_url, persistent_id, api_token):
@@ -12,11 +12,20 @@ def extract_dataverse_ids(dataverse_url, persistent_id, api_token):
     response = requests.get(api_endpoint, headers=headers)
     if response.status_code == 200:
         datasets = response.json()
-        dois = [dataset['persistentUrl'] for dataset in datasets["data"] if
-                'persistentUrl' in dataset]
-        print(dois)
+        doi_list = [reformat_doi(dataset['persistentUrl'])
+                    for dataset in datasets["data"]
+                    if 'persistentUrl' in dataset
+                    and 'publicationDate' not in dataset]
+        print(doi_list)
+        print(len(doi_list))
+        return doi_list
     else:
         print(f"Request failed with status code: {response.status_code}")
+
+
+def reformat_doi(unstructured_doi):
+    doi = 'doi:' + unstructured_doi.split("/", 3)[3]
+    return doi
 
 
 if __name__ == '__main__':
--- dataverse_utils/publish_dataverse.py
+++ dataverse_utils/publish_dataverse.py
@@ -1,9 +1,7 @@
 # Requires pyDataverse 0.3.1
 import argparse
-
 from pyDataverse.api import NativeApi
-from pyDataverse.utils import dataverse_tree_walker
-
+from extract_dataverse_ids import extract_dataverse_ids
 
 parser = argparse.ArgumentParser(description="Publish all datasets inside a given dataverse")
 parser.add_argument('--dataverse', type=str, dest='dataverse_url', help='The dataverse url, FQDN format')
@@ -11,13 +9,11 @@ parser.add_argument('--token', type=str, dest='dataverse_token', help='The datav
 parser.add_argument('--dataverse_parent', type=str, dest='dataverse_parent', help='The Dataverse identifier')
 args = parser.parse_args()
 api = NativeApi(base_url=args.dataverse_url, api_token=args.dataverse_token)
-children = api.get_children(parent=args.dataverse_parent, children_types=['datasets'])
-pids = []
-for child in children:
-    pids.append(child['pid'])
-    print("Pids identified: ", pids)
-    print("Amount of pids: ", len(pids))
-    for pid in pids:
-        data = api.get_dataset(pid)
-        resp = api.publish_dataset(pid=pid, release_type="major")
-        print(resp.json())  
+pids = extract_dataverse_ids(dataverse_url=args.dataverse_url,
+                             persistent_id=args.dataverse_parent,
+                             api_token=args.dataverse_token)
+
+for pid in pids:
+    data = api.get_dataset(pid)
+    resp = api.publish_dataset(pid=pid, release_type="major")
+    print(resp.json())
