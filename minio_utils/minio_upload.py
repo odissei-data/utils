12fdf05884be7570010f38d7ea8fee297e0498ae
Fjodor van Rijsselberg <fjodorvanrijsselberg@Fjodors-MBP.ia.knaw.local>
2023-11-27 09:39:59 +0100

    Updated minio upload script.

--- minio_utils/minio_upload.py
+++ minio_utils/minio_upload.py
@@ -11,8 +11,8 @@ parser.add_argument('--password', type=str, dest='minio_password',
                     help='password')
 parser.add_argument('--path', type=str, dest='path',
                     help='Path to where the data you want to upload lives')
-parser.add_argument('--prefix', type=str, dest='prefix',
-                    help='Object prefix used in the bucket')
+parser.add_argument('--bucket', type=str, dest='bucket',
+                    help='Bucket')
 args = parser.parse_args()
 
 minio_client = boto3.client('s3',
@@ -22,14 +22,21 @@ minio_client = boto3.client('s3',
                             )
 
 local_directory = args.path
-bucket_name = 'harvested-metadata'
-object_prefix = args.prefix
+bucket_name = args.bucket
+success_count = 0
 
 for root, dirs, files in os.walk(local_directory):
     for file in files:
         if not file.startswith('.DS_Store'):
             local_path = os.path.join(root, file)
             relative_path = os.path.relpath(local_path, local_directory)
-            s3_path = os.path.join(object_prefix, relative_path).replace('\\',
-                                                                         '/')
-            minio_client.upload_file(local_path, bucket_name, s3_path)
+            s3_path = relative_path.replace('\\', '/')
+
+            try:
+                minio_client.upload_file(local_path, bucket_name, s3_path)
+                success_count += 1
+                print(f"Uploaded: {local_path}")
+            except Exception as e:
+                print(f"Failed to upload: {local_path}. Error: {str(e)}")
+
+print(f"Total files uploaded: {success_count}")
