bf22760861fbf00db2e248fa9eeca6ec81623639
FjodorvRijsselberg <97509197+FjodorvRijsselberg@users.noreply.github.com>
2024-06-27 09:20:36 +0200

    Update example.config.py

--- dataverse_utils/example.config.py
+++ dataverse_utils/example.config.py
@@ -1,6 +1,3 @@
 API_TOKEN = 'API_TOKEN'
 PRODUCTION_API_TOKEN = ''
 ROOT = 'https://portal.staging.odissei.nl'
-
-## command for minio upload
-## python minio_upload.py --url https://minioapi2.labs.dans.knaw.nl --username minioadmin --password m38dZ93m@3m --path /Users/fjodorvanrijsselberg/Projects/docker-prefect/local-metadata/ODISSEI_Export_20230310 --bucket easy-metadata
